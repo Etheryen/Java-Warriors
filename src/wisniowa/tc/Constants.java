5850b83dec72365d3ddab1316fb2a5d48c72b710
Uczeń <lukasz.ciwoniuk@gmail.com>
2023-03-31 09:31:06 +0200

    fix wrong max bottom position

--- src/wisniowa/tc/Constants.java
+++ src/wisniowa/tc/Constants.java
@@ -8,8 +8,7 @@ public abstract class Constants {
     public static final int MAX_LEFT_POSITION = 0;
     public static final int MAX_RIGHT_POSITION = WINDOW_WIDTH - PLAYER_IMG_WIDTH;
     public static final int MAX_TOP_POSITION = 0;
-    public static final int MAX_BOTTOM_POSITION = WINDOW_WIDTH - PLAYER_IMG_HEIGHT;
-
+    public static final int MAX_BOTTOM_POSITION = WINDOW_HEIGHT - PLAYER_IMG_HEIGHT;
 
     public static final String STATIC_FOLDER = "static/";
     public static final String IMAGES_FOLDER = STATIC_FOLDER + "images/";
